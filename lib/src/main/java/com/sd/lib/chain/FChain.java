4047dca14e758c79830b4bf6b1cff61a357b456b
zjun <565061763@qq.com>
2021-07-07 10:43:29 +0800

    check node state when nextNode called

--- lib/src/main/java/com/sd/lib/chain/FChain.java
+++ lib/src/main/java/com/sd/lib/chain/FChain.java
@@ -72,10 +72,6 @@ public class FChain {
             throw new RuntimeException("current node:" + mCurrentNode + " call node:" + node);
         }
 
-        if (node.getState() == NodeState.None) {
-            throw new RuntimeException("nextNode called when node state is none " + node);
-        }
-
         final int nextIndex = mCurrentIndex + 1;
         if (nextIndex >= mListNode.size()) {
             return;
@@ -185,6 +181,10 @@ public class FChain {
         public final void nextNode() {
             if (_state == NodeState.Finish) {
                 return;
+            }
+
+            if (_state == NodeState.None) {
+                throw new RuntimeException("nextNode called when node state is none " + this);
             }
 
             setState(NodeState.Finish);

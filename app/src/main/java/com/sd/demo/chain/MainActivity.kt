4a5e69bc11dc464bbb6da2b39fe1fb7ee4641dbe
zhengjun <565061763@qq.com>
2021-08-26 13:02:52 +0800

    style: 💄 change style

--- app/src/main/java/com/sd/demo/chain/MainActivity.kt
+++ app/src/main/java/com/sd/demo/chain/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         _chain.apply {
             this.add(_node1)
             this.add(_node2)

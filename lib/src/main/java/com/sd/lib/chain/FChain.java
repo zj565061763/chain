2fdd899528b44c777c5c9caea8ce812732b06db1
zhengjun <565061763@qq.com>
2021-09-26 08:52:57 +0800

    refactor

--- lib/src/main/java/com/sd/lib/chain/FChain.java
+++ lib/src/main/java/com/sd/lib/chain/FChain.java
@@ -18,7 +18,7 @@ public class FChain {
     private Node mCurrentNode = null;
 
     /** 是否正在分发取消事件 */
-    private boolean mIsDispatchCancel = false;
+    private volatile boolean mIsDispatchCancel = false;
 
     private final Handler mHandler = new Handler(Looper.getMainLooper());
 
@@ -32,12 +32,17 @@ public class FChain {
     /**
      * 添加节点，一个节点对象只能添加到一个链上
      */
-    public synchronized void add(@NonNull Node node) {
-        if (mIsDispatchCancel) {
-            throw new RuntimeException("Cannot add node when canceling.");
+    public void add(@NonNull Node node) {
+        synchronized (this) {
+            if (mIsDispatchCancel) {
+                throw new RuntimeException("Cannot add node when canceling.");
+            }
         }
+
         node.init(this, mHandler);
-        mListNode.add(node);
+        synchronized (this) {
+            mListNode.add(node);
+        }
     }
 
     /**
@@ -49,7 +54,6 @@ public class FChain {
         if (mListNode.isEmpty()) {
             return false;
         }
-
         if (mCurrentNode != null) {
             // 已经开始了，不处理
             return false;

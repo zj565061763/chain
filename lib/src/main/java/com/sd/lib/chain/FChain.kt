97612acfd85bd4c39ff2864f64e4d1f237c17346
zj565061763 <565061763@qq.com>
2023-06-08 11:33:04 +0800

    refactor to kotlin

--- lib/src/main/java/com/sd/lib/chain/FChain.kt
+++ lib/src/main/java/com/sd/lib/chain/FChain.kt
@@ -1,275 +1,200 @@
-package com.sd.lib.chain;
+package com.sd.lib.chain
 
-import android.os.Handler;
-import android.os.Looper;
+import android.os.Handler
+import android.os.Looper
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+open class FChain {
+    /** 节点链  */
+    private val _nodeHolder: MutableList<Node> = mutableListOf()
+    private val _handler = Handler(Looper.getMainLooper())
 
-import java.util.ArrayList;
-import java.util.List;
+    /** 当前执行的节点位置  */
+    private var _currentIndex = -1
+    /** 当前执行的节点  */
+    private var _currentNode: Node? = null
 
-public class FChain {
-    /** 节点链 */
-    private final List<Node> mListNode = new ArrayList<>();
-    private final Handler mHandler = new Handler(Looper.getMainLooper());
-
-    /** 当前执行的节点位置 */
-    private int mCurrentIndex = -1;
-
-    /** 当前执行的节点 */
-    private Node mCurrentNode = null;
-
-    /** 是否已经通知了{@link #onFinish()} */
-    private boolean mHasNotifyFinish = false;
+    /** 是否已经通知了[onFinish]  */
+    private var _hasNotifyFinish = false
 
     /**
      * 节点数量
      */
-    public final synchronized int size() {
-        return mListNode.size();
-    }
-
-    /**
-     * 返回当前节点
-     */
-    @Nullable
-    public final synchronized Node getCurrentNode() {
-        return mCurrentNode;
+    @Synchronized
+    fun size(): Int {
+        return _nodeHolder.size
     }
 
     /**
      * 添加节点，一个节点对象只能添加到一个链上。
      */
-    public void add(@NonNull Node node) {
-        synchronized (FChain.this) {
-            node.init(FChain.this, mHandler);
-            mListNode.add(node);
-        }
+    @Synchronized
+    fun add(node: Node) {
+        node.init(this@FChain, _handler)
+        _nodeHolder.add(node)
     }
 
     /**
      * 开始
-     *
      * @return true-本次调用触发了开始
      */
-    public boolean start() {
-        synchronized (FChain.this) {
-            if (mListNode.isEmpty()) return false;
-            if (mCurrentNode != null) return false;
+    @Synchronized
+    fun start(): Boolean {
+        if (_nodeHolder.isEmpty()) return false
+        if (_currentNode != null) return false
 
-            final int index = 0;
-            final Node node = mListNode.get(index);
-            if (node.getState() != NodeState.None) {
-                throw new RuntimeException("Illegal node state " + node.getState());
-            }
+        // 重置为false
+        _hasNotifyFinish = false
 
-            // 重置为false
-            mHasNotifyFinish = false;
-
-            mCurrentIndex = index;
-            mCurrentNode = node;
-            mCurrentNode.notifyRun();
-            return true;
-        }
+        _nodeHolder[0].also {
+            _currentIndex = 0
+            _currentNode = it
+        }.notifyRun()
+        return true
     }
 
     /**
-     * 取消当前节点，并清空所有节点。
+     * 取消，并清空所有节点。
      */
-    public void cancel() {
-        synchronized (FChain.this) {
-            if (mCurrentNode == null) return;
-
-            mCurrentNode.notifyCancel();
-            mCurrentNode = null;
-            mCurrentIndex = -1;
-            mListNode.clear();
-
-            notifyOnFinishLocked();
-        }
+    @Synchronized
+    fun cancel() {
+        val node = _currentNode ?: return
+        node.notifyCancel()
+        _currentNode = null
+        _currentIndex = -1
+        _nodeHolder.clear()
+        notifyOnFinishLocked()
     }
 
-    private synchronized void runNextNode() {
-        final Node currentNode = mCurrentNode;
-        if (currentNode == null) {
-            // 还未开始，不允许调用此方法
-            return;
+    @Synchronized
+    private fun runNextNode() {
+        val node = _currentNode ?: return
+        if (node.state != NodeState.Finish) {
+            error("Current node has not finished " + node.state)
         }
 
-        if (currentNode.getState() != NodeState.Finish) {
-            throw new RuntimeException("Current node has not finished " + currentNode.getState());
+        val nextIndex = _currentIndex + 1
+        if (nextIndex >= _nodeHolder.size) {
+            notifyOnFinishLocked()
+            return
         }
 
-        final int nextIndex = mCurrentIndex + 1;
-        if (nextIndex >= mListNode.size()) {
-            notifyOnFinishLocked();
-            return;
-        }
-
-        mCurrentIndex = nextIndex;
-        mCurrentNode = mListNode.get(nextIndex);
-        mCurrentNode.notifyRun();
+        _nodeHolder[nextIndex].also {
+            _currentIndex = nextIndex
+            _currentNode = it
+        }.notifyRun()
     }
 
-    private void notifyOnFinishLocked() {
-        if (mHasNotifyFinish) return;
-        mHasNotifyFinish = true;
-
-        mHandler.post(new Runnable() {
-            @Override
-            public void run() {
-                onFinish();
-            }
-        });
+    private fun notifyOnFinishLocked() {
+        if (!_hasNotifyFinish) {
+            _hasNotifyFinish = true
+            _handler.post { onFinish() }
+        }
     }
 
     /**
      * 结束回调，在主线程触发。
      */
-    protected void onFinish() {
-    }
+    protected open fun onFinish() {}
 
-    public enum NodeState {
+    enum class NodeState {
         None,
         Run,
-        Finish,
+        Finish
     }
 
-    public static abstract class Node {
-        private FChain _chain;
-        private Handler _handler;
-        private volatile NodeState _state = NodeState.None;
-        private volatile boolean _hasRun;
+    abstract class Node {
+        private lateinit var _chain: FChain
+        private lateinit var _handler: Handler
 
-        /**
-         * 节点状态
-         */
-        @NonNull
-        public final NodeState getState() {
-            return _state;
-        }
+        @Volatile
+        var state = NodeState.None
+            private set
 
-        private synchronized void init(@NonNull FChain chain, @NonNull Handler handler) {
-            if (_chain == null) {
-                _chain = chain;
-                _handler = handler;
-            } else {
-                throw new IllegalArgumentException("Node has been added to " + _chain);
+        @Volatile
+        private var _hasRun = false
+            set(value) {
+                require(value) { "Require true value." }
+                field = value
+            }
+
+        internal fun init(chain: FChain, handler: Handler) {
+            synchronized(this@Node) {
+                if (this::_chain.isInitialized) error("Node has been added to $_chain")
+                _chain = chain
+                _handler = handler
             }
         }
 
-        private synchronized void checkInit() {
-            if (_chain == null) {
-                throw new RuntimeException("Node has not been added to any chain.");
-            }
-            if (_handler == null) {
-                throw new RuntimeException("Node's handler is null.");
-            }
-        }
-
-        private void notifyRun() {
-            checkInit();
-
-            synchronized (Node.this) {
-                if (_state == NodeState.None) {
-                    _state = NodeState.Run;
+        internal fun notifyRun() {
+            synchronized(this@Node) {
+                if (state == NodeState.None) {
+                    state = NodeState.Run
                 } else {
-                    throw new RuntimeException("Illegal node state " + _state);
+                    error("Illegal node state $state")
                 }
             }
 
-            _handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    // 检查状态，如果未被取消，则通知回调
-                    if (_state == NodeState.Run) {
-                        _hasRun = true;
-                        onRun();
-                    }
+            _handler.post {
+                if (state == NodeState.Run) {
+                    _hasRun = true
+                    onRun()
                 }
-            });
+            }
         }
 
-        private void notifyCancel() {
-            checkInit();
-
-            boolean stateChanged = false;
-            synchronized (Node.this) {
-                if (_state != NodeState.Finish) {
-                    _state = NodeState.Finish;
-                    stateChanged = true;
+        internal fun notifyCancel() {
+            val stateChanged = synchronized(this@Node) {
+                if (state != NodeState.Finish) {
+                    state = NodeState.Finish
+                    true
+                } else {
+                    false
                 }
             }
 
-            if (!stateChanged) {
-                // 状态未改变，不处理
-                return;
-            }
-
-            if (_hasRun) {
-                _handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        try {
-                            onCancel();
-                        } finally {
-                            onFinish();
-                        }
-                    }
-                });
+            if (stateChanged && _hasRun) {
+                _handler.post { onCancel() }
+                _handler.post { onFinish() }
             }
         }
 
         /**
          * 执行下一个节点
          */
-        protected final void nextNode() {
-            checkInit();
-
-            boolean stateChanged = false;
-            synchronized (Node.this) {
-                if (_state == NodeState.Run) {
-                    _state = NodeState.Finish;
-                    stateChanged = true;
+        protected fun nextNode() {
+            val stateChanged = synchronized(this@Node) {
+                if (state == NodeState.None) error("Can not call nextNode() before onRun() invoked.")
+                if (state == NodeState.Run) {
+                    state = NodeState.Finish
+                    true
+                } else {
+                    false
                 }
             }
 
-            if (!stateChanged) {
-                // 状态未改变，不处理
-                return;
+            if (stateChanged) {
+                if (_hasRun) {
+                    _handler.post { onFinish() }
+                }
+                _chain.runNextNode()
             }
-
-            if (_hasRun) {
-                _handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        onFinish();
-                    }
-                });
-            }
-
-            // 通知下一个节点
-            _chain.runNextNode();
         }
 
         /**
          * 节点执行回调，主线程触发
          */
-        protected abstract void onRun();
+        protected abstract fun onRun()
 
         /**
          * 节点取消回调，主线程触发。
-         * 如果{@link #onRun()}未触发过，则此方法不会被触发。
+         * 如果[onRun]未触发过，则此方法不会被触发。
          */
-        protected void onCancel() {
-        }
+        protected open fun onCancel() {}
 
         /**
          * 节点结束回调，主线程触发。
-         * 如果{@link #onRun()}未触发过，则此方法不会被触发。
+         * 如果[onRun]未触发过，则此方法不会被触发。
          */
-        protected void onFinish() {
-        }
+        protected open fun onFinish() {}
     }
 }

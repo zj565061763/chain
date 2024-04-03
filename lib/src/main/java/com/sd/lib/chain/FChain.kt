b74983d0a2be9b470c838ffd5cd7fd635b8823d2
zjun <565061763@qq.com>
2024-04-03 21:48:42 +0800

    simplify

--- lib/src/main/java/com/sd/lib/chain/FChain.kt
+++ lib/src/main/java/com/sd/lib/chain/FChain.kt
@@ -4,14 +4,15 @@ import android.os.Handler
 import android.os.Looper
 
 open class FChain {
-    /** 节点链  */
+    /** 所有节点  */
     private val _nodes: MutableList<Node> = mutableListOf()
-    private val _handler = Handler(Looper.getMainLooper())
 
     /** 当前执行的节点位置  */
     private var _currentIndex = -1
     /** 当前执行的节点  */
     private var _currentNode: Node? = null
+
+    private val _handler = Handler(Looper.getMainLooper())
 
     /**
      * 是否正在执行中
@@ -86,7 +87,7 @@ open class FChain {
     }
 
     /**
-     * 结束回调，在主线程触发。
+     * 结束回调，在主线程触发
      */
     protected open fun onFinish() = Unit
 
@@ -104,27 +105,20 @@ open class FChain {
         private var _hasRun = false
 
         internal fun init(chain: FChain, handler: Handler) {
-            synchronized(this@Node) {
-                if (this::_chain.isInitialized) error("Node has been added to $_chain")
-                _chain = chain
-                _handler = handler
-            }
+            if (this::_chain.isInitialized) error("Node has been added to $_chain")
+            _chain = chain
+            _handler = handler
         }
 
         internal fun checkFinish() {
-            synchronized(this@Node) {
-                check(_state == NodeState.Finish) { "Node has not finished with state:${_state}" }
-            }
+            check(_state == NodeState.Finish) { "Node has not finished with state:${_state}" }
         }
 
         internal fun notifyRun() {
-            synchronized(this@Node) {
-                check(_state == NodeState.None) { "Illegal node state $_state" }
-                _state = NodeState.Run
-            }
-
+            check(_state == NodeState.None)
+            _state = NodeState.Run
             _handler.post {
-                synchronized(this@Node) {
+                synchronized(_chain) {
                     (_state == NodeState.Run).also {
                         if (it) _hasRun = true
                     }
@@ -137,15 +131,9 @@ open class FChain {
         }
 
         internal fun notifyCancel() {
-            synchronized(this@Node) {
-                if (_state != NodeState.Finish) {
-                    _state = NodeState.Finish
-                    _hasRun
-                } else {
-                    false
-                }
-            }.let { notify ->
-                if (notify) {
+            if (_state != NodeState.Finish) {
+                _state = NodeState.Finish
+                if (_hasRun) {
                     _handler.post { onCancel() }
                     _handler.post { onFinish() }
                 }
@@ -156,20 +144,19 @@ open class FChain {
          * 执行下一个节点
          */
         protected fun nextNode() {
-            synchronized(this@Node) {
+            check(this::_chain.isInitialized) { "Node has not been initialized." }
+            synchronized(_chain) {
                 when (_state) {
-                    NodeState.None -> error("Can not call nextNode() before onRun() invoked.")
+                    NodeState.None -> error("Can not call nextNode() before onRun().")
                     NodeState.Finish -> return
                     NodeState.Run -> {
                         _state = NodeState.Finish
-                        _hasRun
+                        if (_hasRun) {
+                            _handler.post { onFinish() }
+                        }
+                        _chain.runNextNode()
                     }
                 }
-            }.let { notify ->
-                if (notify) {
-                    _handler.post { onFinish() }
-                }
-                _chain.runNextNode()
             }
         }
 
@@ -179,14 +166,14 @@ open class FChain {
         protected abstract fun onRun()
 
         /**
-         * 节点取消回调，主线程触发。
-         * 如果[onRun]未触发过，则此方法不会被触发。
+         * 节点取消回调，主线程触发，
+         * 如果[onRun]未触发过，则此方法不会被触发
          */
         protected open fun onCancel() = Unit
 
         /**
-         * 节点结束回调，主线程触发。
-         * 如果[onRun]未触发过，则此方法不会被触发。
+         * 节点结束回调，主线程触发，
+         * 如果[onRun]未触发过，则此方法不会被触发
          */
         protected open fun onFinish() = Unit
     }
